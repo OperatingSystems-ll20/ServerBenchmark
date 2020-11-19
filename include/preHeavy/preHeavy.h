7e7b739707bfb878725c59cc6c24f53cac5f91d2
Juan Pablo Brenes <juanp1995@Laptop-Debian>
2020-11-19 02:37:18 -0600

    Sockets and termination of server execution

--- include/preHeavy/preHeavy.h
+++ include/preHeavy/preHeavy.h
@@ -3,10 +3,15 @@
 
 #define TRUE 1
 #define FALSE 0
+#define MAX_IMAGES 100
+#define PORT 9000
+
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 typedef struct Data {
     int _busy;
     int _doWork;
+    int _terminate;
     int _socket;
     // pthread_condattr_t _condAttr;
     // pthread_cond_t _cond;
@@ -20,8 +25,9 @@ int _childID;
 int _exitLoop;
 
 /** Shared **/
-Data **_sharedData;
-pthread_mutex_t _mutex;
+Data *_childsData;
+int *_processedImages;
+pthread_mutex_t *_mutex;
 pthread_mutexattr_t _mutexAttr;
 
 static int getArgs(const int pArgc, char *pArgv[]);
@@ -33,6 +39,9 @@ static void doWork();
 
 static void createSharedData();
 static void createProcesses();
+static void killProcesses();
+static void unmapSharedMem();
+static void acceptConnections();
 
 
 
--- src/preHeavy/preHeavy.c
+++ src/preHeavy/preHeavy.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h> 
 #include <sys/mman.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include <unistd.h> 
 #include <preHeavy.h>
@@ -25,8 +31,17 @@ static void handleSigCont(){}
 
 static void doWork(){
     while (!_exitLoop){
-        pause();
-        printf("Child %d waked up!!!\n", _childID);
+        while(!_childsData[_childID]._doWork && !_childsData[_childID]._terminate) 
+            pause();
+
+        // pthread_mutex_lock(_mutex);
+        // if(*_processedImages < MAX_IMAGES-1){
+        //     (*_processedImages)++;
+        //     printf("Process %d incrementing counter. Counter = %d\n", _childID, *_processedImages);
+        // }
+        // pthread_mutex_unlock(_mutex);
+
+        _childsData[_childID]._doWork = 0;
     }
     
     printf("[son] pid %d with _childID %d exiting...\n", getpid(), _childID);
@@ -37,20 +52,25 @@ static void createSharedData(){
     int protection = PROT_READ | PROT_WRITE;
     int visibility = MAP_SHARED | MAP_ANONYMOUS;
 
-    _sharedData = mmap(NULL, sizeof(Data)*_nProcesses, protection, visibility, -1, 0);
+    _processedImages = (int*)mmap(NULL, sizeof(int), protection, visibility, -1, 0);
+    _mutex = mmap(NULL, sizeof(pthread_mutex_t), protection, visibility, -1, 0);
+    _childsData = mmap(NULL, sizeof(Data)*_nProcesses, protection, visibility, -1, 0);
+
+    *_processedImages = 0;
 
     for(int i = 0; i < _nProcesses; i++){
-        _sharedData[i]->_busy = FALSE;
-        _sharedData[i]->_doWork = FALSE;
-        _sharedData[i]->_socket = 0;
-        // pthread_condattr_init(&_sharedData[i]->_condAttr);
-        // pthread_condattr_setpshared(&_sharedData[i]->_condAttr, PTHREAD_PROCESS_SHARED);
-        // pthread_cond_init(&_sharedData[i]->_cond, &_sharedData[i]->_condAttr);
+        _childsData[i]._busy = FALSE;
+        _childsData[i]._doWork = FALSE;
+        _childsData[i]._terminate = FALSE;
+        _childsData[i]._socket = 0;
+        // pthread_condattr_init(&_childsData[i]->_condAttr);
+        // pthread_condattr_setpshared(&_childsData[i]->_condAttr, PTHREAD_PROCESS_SHARED);
+        // pthread_cond_init(&_childsData[i]->_cond, &_childsData[i]->_condAttr);
     }
 
     pthread_mutexattr_init(&_mutexAttr);
     pthread_mutexattr_setpshared(&_mutexAttr, PTHREAD_PROCESS_SHARED);
-    pthread_mutex_init(&_mutex, &_mutexAttr);
+    pthread_mutex_init(_mutex, &_mutexAttr);
 }
 
 
@@ -68,6 +88,96 @@ static void createProcesses(){
     }
 }
 
+static void killProcesses(){
+    for(int i = 0; i < _nProcesses; i++){
+        _childsData[i]._terminate = 1;
+        kill(_childs[i], SIGTERM);
+        wait(NULL);
+    }
+}
+
+static void unmapSharedMem(){
+    munmap(_processedImages, sizeof(int));
+    munmap(_mutex, sizeof(pthread_mutex_t));
+    munmap(_childsData, sizeof(Data)*_nProcesses);
+}
+
+
+static void acceptConnections(){
+    int socketFD, newSocketFD, ret;
+    socklen_t clientLen;
+    int exitLoop = 0;
+    struct sockaddr_in serverAddress, clientAddress;
+    char input[100];
+
+    fd_set readFds;
+
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket < 0) {
+        fprintf(stderr, "Error: Can't open the socket\n");
+        exit(-1);
+    }
+
+    bzero((char *) &serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+    if(bind(socketFD, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
+        fprintf(stderr, "Error: Binding socket\n");
+        exit(-1);
+    }
+    listen(socketFD,5);
+    clientLen = sizeof(clientAddress);
+
+    FD_ZERO(&readFds);
+    FD_SET(STDIN_FILENO, &readFds);
+    FD_SET(socketFD, &readFds);
+
+    while(!exitLoop){
+        int available = select(socketFD+1, &readFds, NULL, NULL, NULL);
+        if(available < 0){
+            printf("Error on select\n");
+            killProcesses();
+            close(newSocketFD);
+            close(socketFD);
+            exit(-1);
+        }
+
+        else{
+            if(FD_ISSET(STDIN_FILENO, &readFds)){
+                scanf("%s", input);
+                if(strcmp(input, "exit") == 0){
+                    printf("Terminating server...\n");
+                    break;
+                }
+            }
+
+            if(FD_ISSET(socketFD, &readFds)){
+                newSocketFD = accept(socketFD, (struct sockaddr *) &clientAddress, &clientLen);
+                if (newSocketFD < 0) {
+                    fprintf(stderr, "Error: Accepting a connection\n");
+                    exitLoop = 1;
+                    continue;
+                }
+
+                for(int i = 0; i < _nProcesses; i++){
+                    if(!_childsData[i]._busy){
+                        _childsData[i]._socket = newSocketFD;
+                        _childsData[i]._doWork = 1;
+                        kill(_childs[i], SIGCONT);
+                    }
+                }
+            }
+        }
+    }
+
+    killProcesses();
+    unmapSharedMem();
+    close(newSocketFD);
+    close(socketFD);
+}
+
 int main(int argc, char *argv[]){
     _nProcesses = getArgs(argc, argv);
     if(!_nProcesses || _nProcesses < 0) {
@@ -75,22 +185,9 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    // createSharedData();
+    createSharedData();
     createProcesses();
-
-    printf("Sleeping main thread!!!\n");
-    sleep(5);
-    printf("Waking child 0 one time!!!\n");
-    kill(_childs[0], SIGCONT);
+    acceptConnections();
     
-    printf("Sleeping main thread!!!\n");
-    sleep(3);
-    printf("Terminating childs!!!\n");
-    for(int i = 0; i < _nProcesses; i++){
-        kill(_childs[i], SIGTERM);
-        wait(NULL);
-    } 
-    
-
     return 0;
 }
